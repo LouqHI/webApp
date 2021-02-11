7647cabcb996a47653bc662e770f2f58dbdfd6ec
LouqHI <h.islamiya75@gmail.com>
2021-02-11 06:49:43 +0100

    test 2  avec salut

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -1,49 +1,51 @@
 const http = require('http')
 const app = require('./app')
 
-const normalisationPort = valeur => {
-    const port = parseInt(valeur,10);
+const port = process.env.PORT || 4000;
+app.listen(port, console.log("Serveur en écoute sur: "+ port))
+// const normalisationPort = valeur => {
+//     const port = parseInt(valeur,10);
 
-    if (isNaN(port)){
-        return valeur
-    }
-    if(port >=0){
-        return port;
-    }
-    return false
-};
+//     if (isNaN(port)){
+//         return valeur
+//     }
+//     if(port >=0){
+//         return port;
+//     }
+//     return false
+// };
 
-//normalisation du port
-const port = normalisationPort( process.env.PORT || '4000') 
-app.set('port', port)
+// //normalisation du port
+// const port = normalisationPort( process.env.PORT || '4000') 
+// app.set('port', port)
 
-//capteur d'erreur serveur
-const errorHandler = error =>{
-    if (error.syscall !== 'listen') {
-        throw error
-    }
-    const address= serveur.address();
-    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port
-    switch (error.code){
-        case 'EACCES':
-            console.error(bind + 'droits utilisateurs supérieurs requis')
-            process.exit(1);
-            break
-        case 'EADDRINUSE':
-            console.error(bind + 'est déjà utilisé');
-            process.exit(1)
-            break;
-        default:
-            throw error
-    }
-}
+// //capteur d'erreur serveur
+// const errorHandler = error =>{
+//     if (error.syscall !== 'listen') {
+//         throw error
+//     }
+//     const address= serveur.address();
+//     const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port
+//     switch (error.code){
+//         case 'EACCES':
+//             console.error(bind + 'droits utilisateurs supérieurs requis')
+//             process.exit(1);
+//             break
+//         case 'EADDRINUSE':
+//             console.error(bind + 'est déjà utilisé');
+//             process.exit(1)
+//             break;
+//         default:
+//             throw error
+//     }
+// }
 
-const serveur = http.createServer(app);
-serveur.on('error', errorHandler);
-serveur.on('listening', ()=>{
-    const address = serveur.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
-    console.log('Serveur en écoute sur ' + bind)
-})
+// const serveur = http.createServer(app);
+// serveur.on('error', errorHandler);
+// serveur.on('listening', ()=>{
+//     const address = serveur.address();
+//     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
+//     console.log('Serveur en écoute sur ' + bind)
+// })
 
-serveur.listen(port);
+// serveur.listen(port);
